512e35c27b20af3e4c81cb776275eda0fb65a979
Aaditya <aadityajujagar@gmail.com>
2026-01-18 03:10:45 +0530

    cors issue fixed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -17,11 +17,10 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173", // local dev
-      "https://optimum-output-assignment.vercel.app/products" // prod frontend
+      "https://optimum-output-assignment.vercel.app", // prod frontend
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    credentials: true
-  })
+  }),
 );
 
 app.get("/", (_req, res) => {
