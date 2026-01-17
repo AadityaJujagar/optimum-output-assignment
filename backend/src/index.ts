4b97cc404357dc56bb7422fd65a2481c06302fe3
Aaditya <aadityajujagar@gmail.com>
2026-01-17 23:14:02 +0530

    cors issue fixed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,6 +14,11 @@ app.use(express.json());
 app.use(cors());
 app.use("/api", routes); // phase 2.4.3: add routes to index
 
+app.use(cors({
+  origin: "https://optimum-output-assignment.onrender.com/",
+  methods: ["GET","POST","PUT","DELETE"],
+}));
+
 app.get("/", (_req, res) => {
   res.send("APP RUNNING...");
 });
