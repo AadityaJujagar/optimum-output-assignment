6e5a79c38cdb83028f2aec06988e6142146e9880
Aaditya <aadityajujagar@gmail.com>
2026-01-18 02:53:55 +0530

    cors issue fixed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,7 +13,9 @@ const PORT = process.env.PORT || 4000;
 app.use(express.json());
 app.use("/api", routes); // phase 2.4.3: add routes to index
 
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 
 app.get("/", (_req, res) => {
   res.send("APP RUNNING...");
